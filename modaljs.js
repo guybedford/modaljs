6c3155555d998b6ef55833ce4089cff3de3e570d
guybedford <guybedford@gmail.com>
2013-04-24 20:33:59 -0700

    first commit

---
+++ modaljs.js
@@ -0,0 +1,19 @@
+define(['hbs!~/modaljs', 'jquery', 'less!~/modaljs'], function(tpl, $) {
+  return {
+    render: tpl,
+    attach: function(el) {
+      var controller = {
+        close: function() {
+          var self = this;
+          $(el).fadeOut(function() {
+            self.dispose();
+          });
+        }
+      };
+      $('.close', el).click(function() {
+        controller.close();
+      });
+      return controller;
+    }
+  };
+})
